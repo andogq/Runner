745bcf2fbe23235b5b6d5564dca23c63192eeb42
Tom Anderson <tom@ando.gq>
2020-07-21 18:00:55 +1000

    Remove unused functions from authentication

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -44,21 +44,10 @@ let authentication = {
             throw({email: emailError, password: passwordError});
         }).finally(() => window.loader.stop(loadId));
     },
-    clearErrors() {
-        [...document.getElementById("container").getElementsByClassName("authentication")].forEach((el) => {
-            [...el.getElementsByClassName("error")].forEach((e) => {
-                e.classList.remove("error");
-                e.parentElement.children[2].innerHTML = "";
-            });
-        });
-    },
     init() {
-        document.getElementById("button_login").addEventListener("click", this.login.bind(this));
-        document.getElementById("button_register").addEventListener("click", this.register.bind(this));
-
         window.firebase.auth().onAuthStateChanged(() => {
             // Check login
-            if (window.firebase.auth().currentUser != null) this.postLogin();
+            console.log("Login change");
         });
     }
 }
